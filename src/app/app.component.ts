b2efd585bcc4ef9b3b5446676ca4fb1932a49da2
kelly.lopes <kelly.cibelly@maidahealth.com>
2021-11-24 16:36:36 -0300

    adicionado metodo para envio de mensagem pelo iframe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularIframe';
+  title = 'Iframe';
+  response: any = {
+    iframeVisible: false,
+  }
+
+  closeIframe(message: string, alertType: string) {
+    window.parent.postMessage(this.facture(message, alertType), '*')
+  }
+
+  facture(message: string, alertType: string) {
+    if (message !== '' && alertType !== '') {
+      this.response['feedback'] = {
+        message,
+        alertType
+      }
+    }
+
+    return this.response;
+  }
 }
